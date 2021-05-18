c40d0161579efd172c55c62f1a51d8abb9deacda
Şahin Karakulak <karakulaksahin@gmail.com>
2021-05-18 16:27:06 +0300

    .

---
+++ app/src/main/java/com/mrcaracal/firebaseuseroperations/Data.kt
@@ -0,0 +1,3 @@
+package com.mrcaracal.firebaseuseroperations
+
+class Data (var data1: String, var data2: String)
--- app/src/main/java/com/mrcaracal/firebaseuseroperations/HomeActivity.kt
+++ app/src/main/java/com/mrcaracal/firebaseuseroperations/HomeActivity.kt
@@ -4,13 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
 import com.mrcaracal.firebaseuseroperations.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
     private lateinit var auth: FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
+    private lateinit var documentReference: DocumentReference
+    private lateinit var datas: HashMap<String, String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +25,12 @@ class HomeActivity : AppCompatActivity() {
         setContentView(view)
 
         auth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
+        documentReference = firestore.collection("Users").document()
+
+        datas = HashMap()
+        datas.put("name", "Caracal")
+        datas.put("country", "Turkey")
     }
 
     fun exit(view: View) {
@@ -27,4 +39,34 @@ class HomeActivity : AppCompatActivity() {
         startActivity(intent)
         finish()
     }
+
+    fun save(view: View) {
+        documentReference.set(datas)
+            .addOnSuccessListener {
+                Toast.makeText(applicationContext, "Successful", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener {
+                Toast.makeText(applicationContext, "Unsuccessful", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    fun getData(view: View) {
+        documentReference.get()
+            .addOnCompleteListener{ mrcaracal ->
+                if (mrcaracal.isSuccessful){
+                    var documentSnapshot = mrcaracal.getResult()
+                    if (documentSnapshot!!.exists()){
+                        var data_name = documentSnapshot.get("name")
+                        var data_country = documentSnapshot.get("country")
+                        Toast.makeText(applicationContext, "Name: "+ data_name, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            .addOnFailureListener {
+                Toast.makeText(applicationContext, "Unsuccessful", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    fun saveCol(view: View) {}
+    fun getDataCol(view: View) {}
 }
